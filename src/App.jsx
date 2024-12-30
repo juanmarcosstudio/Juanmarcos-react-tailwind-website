22e80e110f54b6dd1e680e6a21300f1b0ca476c0
Jhon Mark <jhonmarktamayo134@gmail.com>
2024-12-30 23:41:05 +0700

    Add: About files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Navbar from './assets/components/Navbar'
 import Hero from './assets/components/Hero'
+import About from './assets/components/About'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <About />
+
     </div>
   )
 }
