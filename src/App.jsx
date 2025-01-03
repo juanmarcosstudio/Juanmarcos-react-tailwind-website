9093bbd9e5a60d6d904719a9ea49ef49f2ef1ac3
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-01-03 15:00:39 +0700

    Add Sevices

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from './assets/components/Navbar'
 import Hero from './assets/components/Hero'
 import About from './assets/components/About'
+import Services from './assets/components/Services'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <About />
+      <Services />
 
     </div>
   )
