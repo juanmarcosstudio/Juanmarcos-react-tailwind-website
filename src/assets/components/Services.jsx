36239de9089e0cfe548ae938a78506e32f213251
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-01-04 14:38:06 +0700

    Services Section

---
+++ src/assets/components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div className='bg-white py-16'>
+      <div className='container mx-auto px-6 lg:px-24 text-center'>
+        <h3>Services</h3>
+        <h2>Check my services</h2>
+      </div>
+    </div>
+  )
+}
+
+export default Services
