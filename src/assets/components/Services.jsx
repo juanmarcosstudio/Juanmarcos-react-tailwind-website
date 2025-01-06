77fa44b478f6a96b36c889395ae35bda3ab28058
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-01-06 15:13:20 +0700

    Add: Explore Servicess button

--- src/assets/components/Services.jsx
+++ src/assets/components/Services.jsx
@@ -1,10 +1,11 @@
 import service1 from "../service1.png"
 import service2 from "../service2.png"
 import service3 from "../service3.png"
+import { FaArrowRight } from 'react-icons/fa'
 
 const Services = () => {
   return (
-    <div className='bg-white py-16'>
+    <div className='bg-white py-16 px-8'>
       <div className='container mx-auto px-6 lg:px-24 text-center'>
         <div className='mb-12'>
             <h3 className='text-blue-500 font-semibold text-lg uppercase mb-2'>Services</h3>
@@ -27,6 +28,13 @@ const Services = () => {
             <h3 className='text-2xl font-semibold text-gray-800 mb-2'>Branding</h3>
             <p className='text-gray-600 mb-4'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque porta imperdiet ultricies. Aliquam tincidunt orci sed felis pretium, ac auctor arcu tincidunt.</p>
         </div>
+      </div>
+      
+      <div className='mt-8 text-right'>
+        <a className='flex items-center justify-end text-blue-700 font-semibold hover:text-blue-500'>
+            Explore All Services <FaArrowRight 
+            className='ml-2' />
+        </a>
       </div>
     </div>
   )
