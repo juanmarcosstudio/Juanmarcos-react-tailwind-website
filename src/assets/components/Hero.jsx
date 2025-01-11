85e954c82378f4d2398066944967c395829eb4ed
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-01-11 23:16:26 +0700

    modify styles

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -4,25 +4,24 @@ import CV from "../cv.pdf"
 
 const Hero = () => {
   return (
-    <div className="text-black flex flex-col-reverse md:flex-row justify-center items-center py-10 px-5 sm:px-10 lg:px-40 bg-gradient-to-t from-white to-yellow-50" >
+    <div className="text-black flex flex-col-reverse md:flex-row justify-center items-center py-10 px-5 sm:px-10 lg:px-40 bg-[#353A3A]" >
       <div className="w-full md:w-1/2 mb-10 text-center md:text-left" >
-        <h3 className="text-base md:text-lg lg:text-3xl mb-0 md:-mb-2 font-bold text-blue-800" >
+        <h3 className="text-base md:text-lg lg:text-3xl mb-0 md:-mb-2 font-bold text-[#eab949]
+        " >
              Hello, I am
         </h3>
-        <h1 className="text-2xl md:text-4xl lg:text-5xl my-0 md:-ml-1 font-bold py-0 md:py-2" >
+        <h1 className="text-2xl text-white md:text-4xl lg:text-5xl my-0 md:-ml-1 font-bold py-0 md:py-2" >
             Jhon Mark
         </h1>
-        <h3 className="text-base md:text-xl lg:text-2xl text-green-700" >
-          Web Developer
-        </h3>
-        <p className="text-sm md:text-xl lg:text-2xl tracking-tighter" >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque feugiat tempus nisl, quis commodo est consectetur id. Aenean vitae mattis massa.
+        
+        <p className="text-sm md:text-xl lg:text-2xl tracking-normal text-white" >
+          I am learning advanced JavaScript, improving my React.js skills for building interactive user interfaces, and working on backend development with Node.js and Express to create efficient and scalable applications.
         </p>
         <div className="flex items-center justify-center md:justify-start gap-4 mt-5" >
-            <button className="bg-blue-800 px-10 py-1 rounded-full mr-4 hover:bg-blue-700 text-white duration-200" >
+            <button className="bg-[#eab949] px-10 py-1 rounded-full mr-4 hover:bg-[#4370d8] text-white duration-200" >
               Contact
             </button>
-            <a className="flex items-center text-blue-800 font-semibold" href={CV} download="CV.pdf">
+            <a className="flex items-center text-[#eab949] font-semibold" href={CV} download="CV.pdf">
                 
                     Download Resume <BsDownload className="ml-2 text-lg" />
                 
