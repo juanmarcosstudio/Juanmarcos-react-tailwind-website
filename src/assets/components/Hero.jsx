5a1b8abf48b8daa100d63be5f101e1300c4124f5
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-01-14 00:50:32 +0700

    feat: Add smooth scroll functionality to Contact button in About section using react-scroll

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -1,8 +1,12 @@
 import { BsDownload } from "react-icons/bs"
 import avatarImg from "../hero.png"
 import CV from "../cv.pdf"
+import { Link } from "react-scroll"
+
 
 const Hero = () => {
+
+
   return (
     <div className="text-black flex flex-col-reverse md:flex-row justify-center items-center py-10 px-5 sm:px-10 lg:px-40 bg-[#353A3A]" >
       <div className="w-full md:w-1/2 mb-10 text-center md:text-left" >
@@ -18,9 +22,16 @@ const Hero = () => {
           I am learning advanced JavaScript, improving my React.js skills for building interactive user interfaces, and working on backend development with Node.js and Express to create efficient and scalable applications.
         </p>
         <div className="flex items-center justify-center md:justify-start gap-4 mt-5" >
-            <button className="bg-[#eab949] px-10 py-1 rounded-full mr-4 hover:bg-[#4370d8] text-white duration-200" >
-              Contact
-            </button>
+          
+              <Link
+                to="contact"
+                smooth={true}
+                duration={500}
+                className="bg-[#eab949] px-10 py-1 rounded-full mr-4 hover:bg-[#4370d8] text-white duration-200 cursor-pointer"
+              >
+                Contact
+              </Link>
+            
             <a className="flex items-center text-[#eab949] font-semibold" href={CV} download="jhonmarkresume.pdf">
                 
                     Download Resume <BsDownload className="ml-2 text-lg" />
