01610b55f2b9cd4cf9f3f3c6d550d1c714ec7b9f
Jhon Mark <jhonmarktamayo134@gmail.com>
2025-01-30 22:18:45 +0700

    Modify: Text

--- src/assets/components/Contact.jsx
+++ src/assets/components/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
           </div>
           <div className='space-y-4 text-white'>
             <FiMapPin className='text-[#eab949] w-6 h-6'/>
-            <span>San Carlos City, Pangasinan, Philippines</span>
+            <span>Pangasinan, Philippines</span>
           </div>
           <div className='space-y-4 text-white'>
             <FiGithub className='text-[#eab949] w-6 h-6'/>
